c95f102fce6aae9fae2fff9f44f05e1f3759233d
Jiffdahli <j.c.martinez@t-online.de>
2026-05-11 08:22:26 +0200

    Components, container and architecture frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
-import Header from '../Components/Header/header'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage";
+import MainPage from "./pages/MainPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <Header />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/main" element={<MainPage />} />
+
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
