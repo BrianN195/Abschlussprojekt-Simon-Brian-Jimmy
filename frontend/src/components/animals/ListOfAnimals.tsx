8fda62ff97625a7f5bf91cf8f04439adac7bd44b
BrianN195 <brianick95@gmail.com>
2026-05-28 15:33:07 +0200

    fertsch

--- frontend/src/components/animals/ListOfAnimals.tsx
+++ frontend/src/components/animals/ListOfAnimals.tsx
@@ -1,15 +1,69 @@
-import { useTranslation } from 'react-i18next';
-import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useEffect, useState, type ChangeEvent } from "react";
 import { speciesService } from "../../services/animalService";
 import type { Animal } from "../../types/Animal";
 import { Link } from "react-router-dom";
 import style from "./AnimalList.module.css";
+import {
+  removeFavoriteAnimal,
+  saveFavoriteAnimal,
+  getFavoriteAnimals,
+} from "../../services/favouritesService";
 
 export default function AnimalList() {
   const { t } = useTranslation();
   const [animals, setAnimals] = useState<Animal[]>([]);
   const [loading, setLoading] = useState(true);
+  // =================================================
+  useEffect(() => {
+    async function loadFavourites() {
+      try {
+        const favourites = await getFavoriteAnimals();
 
+        setIsFavourite(favourites.map((fav) => fav.id));
+      } catch (error) {
+        console.error("Failed to load favourites:", error);
+      }
+    }
+
+    loadFavourites();
+
+    window.addEventListener("favourites-changed", loadFavourites);
+
+    return () => {
+      window.removeEventListener("favourites-changed", loadFavourites);
+    };
+  }, []);
+  // const [animal, setAnimal] = useState<AnimalDetail | null>(null);
+  const [isFavourite, setIsFavourite] = useState<number[]>([]);
+  const handleFavouriteChange = async (
+    event: ChangeEvent<HTMLInputElement>,
+    animal: Animal,
+  ) => {
+    const checked = event.target.checked;
+
+    try {
+      if (checked) {
+        setIsFavourite((prev) => [...prev, animal.id]);
+
+        await saveFavoriteAnimal({
+          id: animal.id,
+          name: animal.name,
+          scientificName: animal.scientificName,
+          imageUrl: animal.imageUrl,
+        });
+
+        return;
+      }
+
+      setIsFavourite((prev) => prev.filter((id) => id !== animal.id));
+
+      await removeFavoriteAnimal(animal.id);
+    } catch (error) {
+      console.error("Failed to update favorite state:", error);
+    }
+  };
+  // =========================================================
   useEffect(() => {
     async function fetchAnimals() {
       try {
@@ -27,7 +81,7 @@ export default function AnimalList() {
     fetchAnimals();
   }, []);
 
-  if (loading) return <p>{t('list.loadingAnimals')}</p>;
+  if (loading) return <p>{t("list.loadingAnimals")}</p>;
 
   return (
     <main className={style.mainContainer}>
@@ -36,17 +90,29 @@ export default function AnimalList() {
           <p>Keine Tiere gespeichert</p>
         ) : (
           animals.map((animal) => (
-            <Link
-              key={animal.id}
-              to={`/animal/${animal.id}`}
-              className={style.cardLink}
-            >
-              <div key={animal.id} className={style.card}>
-                <h3 className="">{animal.name}</h3>
-                <img src={animal.imageUrl} alt="" className={style.pic} />
-                <p className={style.categoryName}>{animal.category}</p>
-              </div>
-            </Link>
+            <div className={style.cardwrapper}>
+              <Link
+                key={animal.id}
+                to={`/animal/${animal.id}`}
+                className={style.cardLink}
+              >
+                <div key={animal.id} className={style.card}>
+                  <h3 className="">{animal.name}</h3>
+                  <div className={style.wrapper}>
+                    <img src={animal.imageUrl} alt="" className={style.pic} />
+                  </div>
+                  <p className={style.categoryName}>{animal.category}</p>
+                </div>
+              </Link>
+              <label className={style.favoriteCheckboxLabel}>
+                <input
+                  type="checkbox"
+                  checked={isFavourite.includes(animal.id)}
+                  onChange={(event) => handleFavouriteChange(event, animal)}
+                />
+                Favorite
+              </label>
+            </div>
           ))
         )}
       </div>
